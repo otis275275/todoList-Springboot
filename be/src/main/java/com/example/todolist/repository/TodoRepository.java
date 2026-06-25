f02d515320a96d09e2d949e52100c40326ff909b
Otis <ledat02072005@gmail.com>
2026-06-25 22:10:29 +0700

    Thành Đạt đã push lên nhánh test

--- be/src/main/java/com/example/todolist/repository/TodoRepository.java
+++ be/src/main/java/com/example/todolist/repository/TodoRepository.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Long> {
     List<Todo> findByUserId(Long userId);
